cbae3bfb9e5c279bea06cc2b36f9c07337b7aadf
Sheila O'Donnell <sheila@contexttravel.com>
2020-04-30 16:36:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ class Formatter {
     let phrase = string.split(" ");
     let exemptWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
     
-    phrase.map((s) => {
-    if (exemptWords.includes(s)) {
-      newArray.push(s)
+    return phrase.map((s) => {
+    if (exemptWords.includes(s) && i !== 0 ) {
+      return s
     }
     else
-      s.charAt(0).toUpperCase() + s.substring(1).join(' ');
-      newArray.push(s);
-  })
+     return s.charAt(0).toUpperCase() + s.substring(1)
+  }).join(" ").toString()
 }
+
