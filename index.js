2db822b857755ec9406f8aa992ec4a4f30fec5c8
Sheila O'Donnell <sheila@contexttravel.com>
2020-04-30 16:34:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  
+  static sanitize(string) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' );
+  }
+  
+  static titleize(string) {
+    let newArray = [];
+    let phrase = string.split(" ");
+    let exemptWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    
+    phrase.map((s) => {
+    if (exemptWords.includes(s)) {
+      newArray.push(s)
+    }
+    else
+      s.charAt(0).toUpperCase() + s.substring(1).join(' ');
+      newArray.push(s);
+  })
+    return newArray
 }
